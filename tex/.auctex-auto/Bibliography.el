208ef76b105ae4c7ea7cc4b664d3675d97bd329f
Tomáš Petit <petitthomas34@gmail.com>
2025-02-07 13:40:48 +0100

    Minor update again Added some tiny portion of theory, will start topological persistence now.

--- tex/.auctex-auto/Bibliography.el
+++ tex/.auctex-auto/Bibliography.el
@@ -7,12 +7,16 @@
     "dey2022computational"
     "rabadan2019topological"
     "hatcher2002algebraic"
+    "gmsh"
     "yu2017ggtree"
     "Borsuk1948OnTI"
     "chazelle1993optimal"
     "2020SciPy-NMeth"
     "dey2013graphinducedcomplexpoint"
     "smajhiVietorisx2013RipsComplex"
-    "saulnechComplex"))
+    "saulnechComplex"
+    "linbox"
+    "kenzo"
+    "perseus"))
  '(or :bibtex :latex))
 
